ce4e879bb81ebccc84c9f454878196c4f611fb96
Fred Suh <50889806+heessuh@users.noreply.github.com>
2022-05-17 14:00:50 -0300

    Update session_origin.spec.js

--- cypress/integration/fred/session_origin.spec.js
+++ cypress/integration/fred/session_origin.spec.js
@@ -1,5 +1,5 @@
 //by using session in beforeEach, one does not have to log in everytime
-//by using origin, one can log in from another site
+//by using origin, one can log into multiple sites using 3rd party auth provider, a new feature of v9.6.0 of Cypress
 
 beforeEach(function () {
   cy.session('login', function () {
